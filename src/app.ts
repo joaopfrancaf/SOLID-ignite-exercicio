e958c497e3aeb24264a8281e444ad8efa60012dd
joao pedro <joaopfrancaf@gmail.com>
2024-02-29 19:17:04 -0300

    refeicao done

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import Fastify from 'fastify'
 import { userRoutes } from './http/controller/user/routes'
+import { refeicaoRoutes } from './http/controller/refeicao/routes'
 
 export const fastify = Fastify()
 
 fastify.register(userRoutes)
+fastify.register(refeicaoRoutes)
